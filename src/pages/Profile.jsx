7572bd5884729245d459e037b7b95749706fe592
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-02-10 12:41:05 +0500

    added signUp and signIn

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,13 @@
+import { getAuth } from 'firebase/auth';
+import { useState, useEffect } from 'react';
+
 const Profile = () => {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+  return user ? <h1>{user.email}</h1> : 'Not Logged In';
 };
 
 export default Profile;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from 'firebase/auth';
+import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
@@ -40,6 +41,12 @@ const SignUp = () => {
       const user = userCredential.user;
 
       updateProfile(auth.currentUser, { displayName: name });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamps = serverTimestamp();
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy);
 
       navigate('/');
     } catch (error) {
