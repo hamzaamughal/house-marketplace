6a3f712131098d0cdfa028642961cd691c16f0d7
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-02-10 11:46:18 +0500

    added firebase auth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,109 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from 'firebase/auth';
+import { db } from '../firebase.config';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
 const SignUp = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, { displayName: name });
+
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
-      <h1>SignUp</h1>
-    </div>
+    <>
+      <div className='pageContainer'>
+        <header>
+          <p className='pageHeader'>Welcome Back!</p>
+        </header>
+        <main>
+          <form onSubmit={onSubmit}>
+            <input
+              type='text'
+              className='nameInput'
+              placeholder='Name'
+              value={name}
+              id='name'
+              onChange={onChange}
+            />
+            <input
+              type='email'
+              className='emailInput'
+              placeholder='Email'
+              value={email}
+              id='email'
+              onChange={onChange}
+            />
+            <div className='passwordInputDiv'>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                className='passwordInput'
+                placeholder='Password'
+                value={password}
+                id='password'
+                onChange={onChange}
+              />
+              <img
+                src={visibilityIcon}
+                alt='show'
+                className='showPassword'
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+            <Link to='/forgot-password' className='forgotPasswordLink'>
+              Forgot Password
+            </Link>
+            <div className='signUpBar'>
+              <p className='signUpText'>Sign Up</p>
+              <button className='signUpButton'>
+                <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+              </button>
+            </div>
+          </form>
+          {/* Google OAuth */}
+          <Link to='/sign-in' className='registerLink'>
+            Sign In Instead
+          </Link>
+        </main>
+      </div>
+    </>
   );
 };
 
