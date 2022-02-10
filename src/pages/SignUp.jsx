bc2407d85d06ffe7c793d7dac04c7b9c3921ea36
Hamza Mughal <66194492+hamzaamughal@users.noreply.github.com>
2022-02-10 16:15:07 +0500

    Google OAuth Added

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,6 +10,7 @@ import { setDoc, doc, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+import OAuth from '../components/OAuth';
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -106,6 +107,7 @@ const SignUp = () => {
             </div>
           </form>
           {/* Google OAuth */}
+          <OAuth />
           <Link to='/sign-in' className='registerLink'>
             Sign In Instead
           </Link>
